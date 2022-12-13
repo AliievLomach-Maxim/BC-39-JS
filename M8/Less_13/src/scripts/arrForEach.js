dfca4739f1488767f38e402871beb0ec8cba54ad
AliievLomachMaxim <aliev.lomach.maxim@gmail.com>
2022-12-13 20:40:35 +0200

    Add less-13

---
+++ M8/Less_13/src/scripts/arrForEach.js
@@ -0,0 +1,10 @@
+export default function arrForEach(arr) {
+	arr.forEach((el) => {
+		el.addEventListener('click', ({ target }) => {
+			console.log('target.classList', target.classList)
+			if (Array.from(target.classList)[0].includes('card'))
+				alert('Click on CARD!')
+			else console.log('Click on', target)
+		})
+	})
+}
